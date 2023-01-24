10393004033f98f03e1c59d8ed5f87f6d823a0dd
dominik.szabo <dominik.szabo@kroneit.com>
2023-01-24 19:32:56 +0100

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,7 +16,7 @@
     <input type="text" id="small-input" v-model="name" placeholder="Insert category name"
       class="block w-50% p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500">
 
-    <button @click="createEmployee()" type="button"
+    <button @click="createCategory()" type="button"
       class="text-white mt-2 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">+
       Add Category</button>
   </div>
@@ -54,7 +54,7 @@ export default {
           console.log("Error getting documents: ", error);
         });
     },
-    createEmployee() {
+    createCategory() {
       db.collection("categories")
         .add({ name: this.name })
         .then(() => {
